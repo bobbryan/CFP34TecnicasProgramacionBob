76c72a8cd8d7330dd2c304eb2f87893fd73f1af7
Maq301 <bryanchangano@gmail.com>
2016-11-29 18:48:02 -0300

    En esta etapa, se transforman las letras de la fila en coordenadas para el juego

--- coordenadas.py
+++ coordenadas.py
@@ -6,3 +6,11 @@ def validarCoordenadas(coordenada):
     coordenadasValidas = ("a1","a2","a3","a4","a5","b1","b2","b3","b4","b5","c1","c2","c3","c4","c5","d1","d2","d3","d4","d5","e1","e2","e3","e4","e5")
     if coordenada in coordenadasValidas :
         return True
+    return False
+
+def transformacionDeCoordenadas(coordenada):
+    diccionarioLetras = {'a':0,'b':1,'c':2,'d':3,'e':4}
+    columna = diccionarioLetras[coordenada[0]]
+    fila = int(coordenada[1]) - 1
+
+    return columna,fila
--- modoPredeterminado.py
+++ modoPredeterminado.py
@@ -5,20 +5,23 @@ import coordenadas
 def jugar():
     print("Bienvenido al juego en modo PREDETERMINADO")
     menu.mostrarTablero(nivelesPredeterminados.primerNivel)
-    print("-----------------------------------")
-    print("       Ingrese una coordena")
-    print("-----------------------------------")
-    print("  Ingresa la FILA, seguido de la COLUMNA")
-    print("-----------------------------------")
+    print("---------------------------------------------------------")
+    print("                 Ingrese una coordena")
+    print("---------------------------------------------------------")
+    print("  Ingresa la FILA (letra), seguido de la COLUMNA (numero)")
+    print("---------------------------------------------------------")
 
 
     coordenadasDelUsuario = coordenadas.pedirCoordenadas()
     while not coordenadas.validarCoordenadas(coordenadasDelUsuario):
-        print("-----------------------------------")
+        print("")
+        print("----------------------------------------------")
         print("    Ingrese una coordena CORRECTA   ")
-        print("-----------------------------------")
+        print("----------------------------------------------")
         coordenadasDelUsuario = coordenadas.pedirCoordenadas()
 
+    traduccionDeCoordenada = coordenadas.transformacionDeCoordenadas(coordenadasDelUsuario)
+    print(traduccionDeCoordenada)
 
-
-
+    print(traduccionDeCoordenada[0])
+    print(traduccionDeCoordenada[1])
